487ddfcbdb83a650148c5fe29b4dd86f46acf62d
pororoarx <kengarrido.legaspi@gmail.com>
2023-05-22 11:17:23 +0800

    Created OperationsDisplay class and print operations

--- calculator_main.py
+++ calculator_main.py
@@ -1,7 +1,13 @@
 # import greeting
 from program_greetings import Greetings
+# import operations
+from display_operations import OperationsDisplay
 
 # create instance of a class - greeting
 greet = Greetings()
+# create instance of a class - operations
+operate = OperationsDisplay()
+
 # call the function
 greet.display()
+operate.operations()
