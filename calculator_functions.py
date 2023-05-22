94efdf5964a4a9caa7d354de54c1f82c4fbaf8ae
pororoarx <kengarrido.legaspi@gmail.com>
2023-05-22 12:54:18 +0800

    if user chooses addition

--- calculator_functions.py
+++ calculator_functions.py
@@ -9,4 +9,14 @@ def calculator():
     # perform a while loop until the user decided to exit the loop
     while True:
         # ask user to enter an operation
-        chosen_operation = input("\n\033[38;5;156mChoose one of the four operations: \033[0m")   
+        chosen_operation = input("\n\033[38;5;156mChoose one of the four operations: \033[0m")  
+
+        # if user chooses addition
+        if chosen_operation == "1":
+            # ask the user to enter two numbers
+            number_1 = input("\033[38;5;214mEnter your first number: \033[0m")
+            number_2 = input("\033[38;5;214mEnter your second number: \033[0m")
+
+            add = Addition()
+            add.addition_operation(number_1, number_2)
+
