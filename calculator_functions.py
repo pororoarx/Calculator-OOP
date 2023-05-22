2813afcec0171d9e06810421cffcb673ac8ef92f
pororoarx <kengarrido.legaspi@gmail.com>
2023-05-22 12:49:20 +0800

    import four operations

---
+++ calculator_functions.py
@@ -0,0 +1,7 @@
+# import four different operations
+from addition import Addition
+from subtraction import Subtraction
+from multiplication import Multiplication
+from division import Division
+
+    
