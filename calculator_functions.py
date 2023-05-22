b911f4e871d94cca2f8a53276bd9fa2cba2aaebb
pororoarx <kengarrido.legaspi@gmail.com>
2023-05-22 12:51:20 +0800

    define a function called calculator

--- calculator_functions.py
+++ calculator_functions.py
@@ -4,4 +4,9 @@ from subtraction import Subtraction
 from multiplication import Multiplication
 from division import Division
 
-    
+# define a function called calculator
+def calculator():
+    # perform a while loop until the user decided to exit the loop
+    while True:
+        # ask user to enter an operation
+        chosen_operation = input("\n\033[38;5;156mChoose one of the four operations: \033[0m")   
