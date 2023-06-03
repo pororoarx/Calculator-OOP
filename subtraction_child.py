30b833091da2f424ff4a8d41d0dd81384a18cc67
pororoarx <kengarrido.legaspi@gmail.com>
2023-06-03 14:42:01 +0800

    proceeded to absolute function

--- subtraction_child.py
+++ subtraction_child.py
@@ -1,6 +1,9 @@
 from subtraction import Subtraction
 
 class ChildSubtract(Subtraction):
-    def diff(self, number_1, number_2):
-        answer = number_1 - number_2
-        return answer
+    def subtraction_operation(self, number_1, number_2):
+        super().subtraction_operation(number_1, number_2)
+
+    def absolute_value(self, number_1, number_2):
+        absolute = abs (number_1 - number_2)
+        print("Absolute value: ", absolute)
