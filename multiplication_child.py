fd38b823e4961888c07eabee5c92a9a1bb3d7cd5
pororoarx <kengarrido.legaspi@gmail.com>
2023-06-03 15:44:51 +0800

    create function for exponent in the child-multiplication class

--- multiplication_child.py
+++ multiplication_child.py
@@ -1,1 +1,9 @@
 from multiplication import Multiplication
+
+class ChildMultiplication(Multiplication):
+    def exponent(self, number_1, number_2):
+        number_1 = float(number_1)
+        number_2 = float(number_2)
+        result = number_1 * number_2
+        print("Exponentiation value: ", result)
+        
