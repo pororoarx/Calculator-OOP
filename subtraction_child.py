d84a56ee7b540a9f7cde5b3b97746b62df443257
pororoarx <kengarrido.legaspi@gmail.com>
2023-06-03 13:22:04 +0800

    edited child-subtract class

--- subtraction_child.py
+++ subtraction_child.py
@@ -1,7 +1,6 @@
 from subtraction import Subtraction
 
 class ChildSubtract(Subtraction):
-    def subtract_absolute(self, number):
-        number = float(number)
-        result = abs(number)
+    def subtract_absolute(self, number_1, number_2):
+        result = number_1 - number_2
         print("Absolute value: ", result)
