b4b2c39f256e19958901a402763f1e1b94dcc671
pororoarx <kengarrido.legaspi@gmail.com>
2023-06-03 15:53:52 +0800

    added try and catch for overflow errors

--- multiplication_child.py
+++ multiplication_child.py
@@ -2,8 +2,13 @@ from multiplication import Multiplication
 
 class ChildMultiplication(Multiplication):
     def exponent(self, number_1, number_2):
-        number_1 = float(number_1)
-        number_2 = float(number_2)
-        result = number_1 ** number_2
-        print("Exponentiation value: ", result)
+        try:
+          number_1 = float(number_1)
+          number_2 = float(number_2)
+          result = number_1 ** number_2
+          print("Exponentiation value: ", result)
+        except ValueError:
+           print("Invalid input")
+        except OverflowError:
+           print("Too large to print output")
         
