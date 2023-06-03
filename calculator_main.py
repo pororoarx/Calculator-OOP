9af0eb47b5a0d99f10868fe513b087a343c0a3ac
pororoarx <kengarrido.legaspi@gmail.com>
2023-06-03 13:20:33 +0800

    call the function in the main file

--- calculator_main.py
+++ calculator_main.py
@@ -4,6 +4,7 @@ from program_greetings import Greetings
 # import calculator functions
 from calculator_functions import calculator
 from addition_child import ChildAddition
+from subtraction_child import ChildSubtract
 
 # import compilations
 
@@ -16,3 +17,6 @@ calculator()
 
 child_1 = ChildAddition()
 child_1.add
+
+child_2 = ChildSubtract()
+child_2.subtract_absolute(number_1, number_2)
