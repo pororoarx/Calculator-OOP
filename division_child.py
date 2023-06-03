f893aebb99fff62c14e0e66d2fe81d7a1112e87a
pororoarx <kengarrido.legaspi@gmail.com>
2023-06-03 16:13:21 +0800

    added function for square root

--- division_child.py
+++ division_child.py
@@ -2,3 +2,12 @@ from division import Division
 import math
 
 class ChildDivision(Division):
+    def square_root(self, number_1, number_2):
+        number_1 = float(number_1)
+        number_2 = float(number_2)
+        
+        result = number_1 / number_2
+        print("Absolute value: ", result)
+
+        square_root = math.sqrt(result)
+        print("The square root of the result is: ", square_root)
