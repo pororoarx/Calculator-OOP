6bf65872f1cad61831c539b1f26e59aeaf271a2c
pororoarx <kengarrido.legaspi@gmail.com>
2023-06-03 16:22:30 +0800

    added exception handling

--- division_child.py
+++ division_child.py
@@ -3,11 +3,20 @@ import math
 
 class ChildDivision(Division):
     def square_root(self, number_1, number_2):
-        number_1 = float(number_1)
-        number_2 = float(number_2)
+        try:
+            number_1 = float(number_1)
+            number_2 = float(number_2)
         
-        result = number_1 / number_2
-        print("Absolute value: ", result)
+            if number_2 == 0:
+                raise ZeroDivisionError("Invalid inpput. Division by zero error.")
+            
+            result = number_1 / number_2
+            print("Result: ", result)
 
-        square_root = math.sqrt(result)
-        print("The square root of the result is: ", square_root)
+            square_root = math.sqrt(result)
+            print("The square root of the result is: ", square_root)
+
+        except ValueError:
+            print("Invalid input")
+        except ZeroDivisionError as error:
+            print(error)
